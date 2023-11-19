c40b6723349b06c429f6c577d20f1f729322c697
MarwanElZaher <118132355+MarwanElZaher@users.noreply.github.com>
2023-11-19 15:03:43 +0200

    props forward to the wrapped elements, in example and tab button components

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -1,6 +1,7 @@
 import TabButton from './TabButton';
 import { EXAMPLES } from '../data';
 import React, { useState } from 'react';
+import Section from './Section.jsx';
 const Example = () => {
   const [selectedTopic, setSelectedTopic] = useState();
   function handleSelect(selectedButton) {
@@ -21,36 +22,35 @@ const Example = () => {
     );
   }
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples" className="Examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === 'components'}
-          onSelect={() => handleSelect('components')}
+          onClick={() => handleSelect('components')}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'jsx'}
-          onSelect={() => handleSelect('jsx')}
+          onClick={() => handleSelect('jsx')}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'props'}
-          onSelect={() => handleSelect('props')}
+          onClick={() => handleSelect('props')}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === 'state'}
-          onSelect={() => handleSelect('state')}
+          onClick={() => handleSelect('state')}
         >
           State
         </TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 };
 export default Example;
